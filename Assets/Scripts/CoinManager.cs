279bee57cb981e9c0f018a6d98f1d00090e4e60b
DSKhrebtov <framzik@gmail.com>
2025-07-15 16:16:03 +0300

    ui

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -1,9 +1,12 @@
+using TMPro;
 using UnityEngine;
 
 public class CoinManager : MonoBehaviour
 {
     [SerializeField] private int _numberCoinsInLevel;
+    [SerializeField] private TextMeshProUGUI _text;
     public void AddOne() { 
         _numberCoinsInLevel ++;
+        _text.text = _numberCoinsInLevel.ToString();
     }
 }
